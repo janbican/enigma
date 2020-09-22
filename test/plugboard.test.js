caaf5b51b048c98abb9f8ac61183f67d461bf4c7
Jan Bican <bican.honza@gmail.com>
2020-09-22 17:26:50 +0200

    plugboard prototype

--- test/plugboard.test.js
+++ test/plugboard.test.js
@@ -2,22 +2,58 @@ import { Plugboard } from './../lib/plugboard.js'
 import { alphabet } from './../lib/alphabet.js'
 
 describe('Plugboard', () => {
+  let plugboard
+
+  beforeEach(() => {
+    plugboard = new Plugboard()
+  })
+
   test('plain alphabet when created', () => {
-    const plugboard = new Plugboard()
     expect(plugboard.alphabet).toBe(alphabet)
   })
 
-  it('alphabet changes when pair is plugged', () => {
-    const plugboard = new Plugboard()
+  test('alphabet changes when pair is plugged', () => {
     plugboard.plug('AZ')
     expect(plugboard.alphabet).toBe('ZBCDEFGHIJKLMNOPQRSTUVWXYA')
   })
 
-  it('alphabet changes when more pairs are plugged', () => {
-    const plugboard = new Plugboard()
+  test('alphabet changes when more pairs are plugged', () => {
     plugboard.plug('MA')
     plugboard.plug('PT')
     plugboard.plug('XQ')
     expect(plugboard.alphabet).toBe('MBCDEFGHIJKLANOTXRSPUVWQYZ')
+  })
+
+  test('plugs changes when letter plugged again', () => {
+    plugboard.plug('GH')
+    plugboard.plug('LA')
+    plugboard.plug('GL')
+    expect(plugboard.alphabet).toBe('ABCDEFLHIJKGMNOPQRSTUVWXYZ')
+  })
+
+  test('alphabet changes when letter is unplugged', () => {
+    plugboard.plug('PT')
+    plugboard.unplug('P')
+    expect(plugboard.alphabet).toBe(alphabet)
+  })
+
+  test('alphabet is plain when unplugAll called', () => {
+    plugboard.plug('MA')
+    plugboard.plug('PT')
+    plugboard.plug('XQ')
+    plugboard.unplugAll()
+    expect(plugboard.alphabet).toBe(alphabet)
+  })
+
+  test('throws error when trying to plug nothing', () => {
+    expect(() => plugboard.plug('')).toThrow(Error)
+  })
+
+  test('throws error when trying to plug invalid pair', () => {
+    expect(() => plugboard.plug('@A')).toThrow(Error)
+  })
+
+  test('throws error when trying to unplug invalid letter', () => {
+    expect(() => plugboard.unplug(1)).toThrow(Error)
   })
 })
