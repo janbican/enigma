3a47f00715dce6747fedad3966d14e0f4b105e08
Jan Bican <bican.honza@gmail.com>
2020-09-22 16:40:16 +0200

    babel added

--- test/plugboard.test.js
+++ test/plugboard.test.js
@@ -1,9 +1,23 @@
-import { Plugboard } from './lib/plugboard'
-import { alphabet } from './lib/alphabet'
+import { Plugboard } from './../lib/plugboard.js'
+import { alphabet } from './../lib/alphabet.js'
 
 describe('Plugboard', () => {
-  it('has plain alphabet when created', () => {
+  test('plain alphabet when created', () => {
     const plugboard = new Plugboard()
     expect(plugboard.alphabet).toBe(alphabet)
+  })
+
+  it('alphabet changes when pair is plugged', () => {
+    const plugboard = new Plugboard()
+    plugboard.plug('AZ')
+    expect(plugboard.alphabet).toBe('ZBCDEFGHIJKLMNOPQRSTUVWXYA')
+  })
+
+  it('alphabet changes when more pairs are plugged', () => {
+    const plugboard = new Plugboard()
+    plugboard.plug('MA')
+    plugboard.plug('PT')
+    plugboard.plug('XQ')
+    expect(plugboard.alphabet).toBe('MBCDEFGHIJKLANOTXRSPUVWQYZ')
   })
 })
